48430bca47b0dcfd1eb55ee90d0dc7d4e0179f6d
Sudirmansyah <sudirmansyah0612@gmail.com>
2023-11-29 20:55:16 +0700

    Move to public

---
+++ src/components/skills.js
@@ -0,0 +1,37 @@
+import bootstrap from "../../public/assets/images/brand/bootstrap.svg"
+import codeigniter from "../../public/assets/images/brand/codeigniter.svg"
+import css from "../../public/assets/images/brand/css.svg"
+import docker from "../../public/assets/images/brand/docker.svg"
+import git from "../../public/assets/images/brand/git.svg"
+import html from "../../public/assets/images/brand/html.svg"
+import javascript from "../../public/assets/images/brand/javascript.svg"
+import mysql from "../../public/assets/images/brand/mysql.svg"
+import nodejs from "../../public/assets/images/brand/nodejs.svg"
+import oracle from "../../public/assets/images/brand/oracle.svg"
+import php from "../../public/assets/images/brand/php.svg"
+import react from "../../public/assets/images/brand/react.svg"
+import tibco from "../../public/assets/images/brand/tibco.svg"
+import lua from "../../public/assets/images/brand/lua-official.svg"
+import postgresql from "../../public/assets/images/brand/postgresql.svg"
+import express from "../../public/assets/images/brand/express.svg"
+import fastify from "../../public/assets/images/brand/fastify.svg"
+
+export {
+    bootstrap,
+    codeigniter,
+    css,
+    docker,
+    git,
+    html,
+    javascript,
+    mysql,
+    nodejs,
+    oracle,
+    php,
+    react,
+    tibco,
+    lua,
+    postgresql,
+    fastify,
+    express
+}
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -20,7 +20,7 @@ import {
     postgresql,
     express,
     fastify
- } from "../assets/skills"
+ } from "../components/skills"
 
 
 const projects = [
